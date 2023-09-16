221fb86dd2a2082ac7daea4b1cf1595ecf0e1a3e
Stoycho Sirakov <stoycho.sirakov@gmail.com>
2023-09-17 00:55:58 +0300

    `

--- 01.StacksAndQueues/EX4.FastFood/Program.cs
+++ 01.StacksAndQueues/EX4.FastFood/Program.cs
@@ -5,7 +5,28 @@ namespace EX4.FastFood
     {
         static void Main()
         {
-            
+            int meals = int.Parse(Console.ReadLine());
+            Queue<int> orders = new Queue<int>(Console.ReadLine().Split().Select(int.Parse));
+            Console.WriteLine(orders.Max());
+            bool foodless = false;
+            int loop = orders.Count();
+            for (int i = 0; i < loop; i++)
+            {
+                if (meals >= orders.Peek())
+                {
+                    meals -= orders.Dequeue();
+                }
+                else
+                {
+                    Console.WriteLine($"Orders left: {string.Join(" ", orders)}");
+                    foodless = true;
+                    break;
+                }
+            }
+            if (!foodless)
+            {
+                Console.WriteLine("Orders complete");
+            }
         }
     }
 }
---
+++ 01.StacksAndQueues/EX5.FashionBoutique/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace EX5.FashionBoutique
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Stack<int> clothes = new Stack<int>(Console.ReadLine().Split().Select(int.Parse));
+            int rackCapacity = int.Parse(Console.ReadLine());
+            int initialClotesCount = clothes.Count();
+            int racksCount = 1;
+            int capacity = 16;
+            for (int i = 0; i < initialClotesCount; i++)
+            {
+                if (clothes.Peek() <= capacity)
+                {
+                    capacity -= clothes.Pop();
+                }
+                else
+                {
+                    racksCount++;
+                    capacity = rackCapacity - clothes.Pop();
+                }
+            }
+            Console.WriteLine(racksCount);
+        }
+    }
+}

bd8c447b0946396bbd407322684aa87dba905b82
Stoycho Sirakov <stoycho.sirakov@gmail.com>
2023-09-16 23:25:55 +0300

    `

---
+++ 01.StacksAndQueues/EX1.BasicStackOperations/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace EX1.BasicStackOperations
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine()
+                .Split()
+                .ToArray();
+            Stack<string> nums = new Stack<string>(Console.ReadLine().Split());
+            for (int i = 1; i <= int.Parse(input[1]); i++) 
+            {
+                nums.Pop();
+            }
+            if (nums.Contains(input[2]))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine(nums.Min());
+            }
+        }
+    }
+}
---
+++ 01.StacksAndQueues/EX2.BasicQueueOperations/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EX2.BasicQueueOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

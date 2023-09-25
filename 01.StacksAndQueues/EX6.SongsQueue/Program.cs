358941ba9cd5b79ea9058fe93feb444978d246a3
Stoycho Sirakov <stoycho.sirakov@gmail.com>
2023-09-26 00:01:21 +0300

    `

---
+++ 01.StacksAndQueues/EX6.SongsQueue/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace EX6.SongsQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var songs = Console.ReadLine()
+                .Split(", ")
+                .ToArray();
+            Queue<string> songsQueue = new Queue<string>();
+            foreach (var item in songs)
+            {
+                songsQueue.Enqueue(item);
+            }
+            while (songsQueue.Count != 0)
+            {
+                var input = Console.ReadLine()
+                    .Split(" ")
+                    .ToArray();
+                if (input[0] == "Add")
+                {
+                    List<string> song = new List<string>();
+                    for (int i = 1; i < input.Length; i++)
+                    {
+                        song.Add(input[i]);
+                    }
+                    if (!songsQueue.Contains(string.Join(" ", song)))
+                    {
+                        songsQueue.Enqueue(string.Join(" ", song));
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{string.Join(" ", song)} is already contained!");
+                    }
+                }
+                else if (input[0] == "Play")
+                {
+                    songsQueue.Dequeue();
+                }
+                else if (input[0] == "Show")
+                {
+                    Console.WriteLine(string.Join(", ", songsQueue));
+                }
+            }
+            Console.WriteLine("No more songs!");
+        }
+    }
+}
---
+++ 01.StacksAndQueues/EX7.TruckTour/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace EX7.TruckTour
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Queue<int[]> pumps = new Queue<int[]>();
+            int pumpsCount = int.Parse(Console.ReadLine());
+            for (int i = 0; i < pumpsCount; i++)
+            {
+                var input = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+                pumps.Enqueue(input);
+            }
+            int bestRoute = 0;
+            while (true)
+            {
+                int totalFuel = 0;
+                foreach (var pump in pumps)
+                {
+                    totalFuel += pump[0];
+                    if (totalFuel - pump[1] < 0)
+                    {
+                        totalFuel = 0;
+                        break;
+                    }
+                    else
+                    {
+                        totalFuel -= pump[1];
+                    }
+                }
+                if (totalFuel > 0)
+                {
+                    break;
+                }
+                bestRoute++;
+                pumps.Enqueue(pumps.Dequeue());
+
+            }
+            Console.WriteLine(bestRoute);
+        }
+    }
+}

372179e354c7b092a6adee15c986324ba4109064
Stoycho Sirakov <stoycho.sirakov@gmail.com>
2023-10-11 00:00:01 +0300

    `

---
+++ 02.MultidimensionalArrays/L3.PrimaryDiagonal/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace L3.PrimaryDiagonal
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int numofRows = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[numofRows, numofRows];
+            for (int i = 0; i < numofRows; i++)
+            {
+                var input = Console.ReadLine()
+                    .Split(" ")
+                    .Select(int.Parse)
+                    .ToArray();
+                for (int y = 0; y < input.Length; y++)
+                {
+                    matrix[i, y] = input[y];
+                }
+            }
+            int diagonalSum = 0;
+            for (int row = 0; row < numofRows; row++)
+            {
+                for (int col = 0; col < numofRows; col++)
+                {
+                    if (col == row)
+                    {
+                        diagonalSum += matrix[row, col];
+                    }
+                }
+            }
+            Console.WriteLine(diagonalSum);
+        }
+    }
+}

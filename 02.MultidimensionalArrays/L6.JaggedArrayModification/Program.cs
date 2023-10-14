4dba90df5c6c145c01e62b61a04c4fdf7357d1ff
Stoycho Sirakov <stoycho.sirakov@gmail.com>
2023-10-14 23:58:05 +0300

    `

--- 02.MultidimensionalArrays/L6.JaggedArrayModification/Program.cs
+++ 02.MultidimensionalArrays/L6.JaggedArrayModification/Program.cs
@@ -4,7 +4,52 @@
     {
         static void Main()
         {
-            
+            int rows = int.Parse(Console.ReadLine());
+            int[][] jagged = new int[rows][];
+            for (int row = 0; row < rows; row++)
+            {
+                var inputRows = Console.ReadLine()
+                    .Split(" ")
+                    .Select(int.Parse)
+                    .ToArray();
+                jagged[row] = new int[inputRows.Length];
+                for (int col = 0; col < inputRows.Length; col++)
+                {
+                    jagged[row][col] = inputRows[col];
+                }
+            }
+            string input;
+            while ((input = Console.ReadLine()) != "END")
+            {
+                var statement = input
+                    .Split(" ")
+                    .ToArray();
+                string command = statement[0];
+                int rowCoordinates = int.Parse(statement[1]);
+                int colCoordinates = int.Parse(statement[2]);
+                int commandValue = int.Parse(statement[3]);
+                //
+                if (rowCoordinates > rows-1 || rowCoordinates < 0 || colCoordinates < 0 || jagged[rowCoordinates].Length < colCoordinates)
+                {
+                    Console.WriteLine("Invalid coordinates");
+                }
+                else if (command == "Add")
+                {
+                    jagged[rowCoordinates][colCoordinates] += commandValue;
+                }
+                else
+                {
+                    jagged[rowCoordinates][colCoordinates] -= commandValue;
+                }
+            }
+            for (int x = 0; x < jagged.GetLength(0); x++)
+            {
+                for (int y = 0; y < jagged[x].Length; y++)
+                {
+                    Console.Write($"{jagged[x][y]} ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
---
+++ 02.MultidimensionalArrays/L7.PascalTriangle/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace L7.PascalTriangle
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int rows = Convert.ToInt32(Console.ReadLine());
+
+            long[][] pascalsTriangle = new long[rows][];
+
+            for (int i = 0; i < rows; i++)
+            {
+                pascalsTriangle[i] = new long[i + 1];
+                for (int j = 0; j <= i; j++)
+                {
+                    if (j == 0 || j == i)
+                    {
+                        pascalsTriangle[i][j] = 1;
+                    }
+                    else
+                    {
+                        pascalsTriangle[i][j] = pascalsTriangle[i - 1][j - 1] + pascalsTriangle[i - 1][j];
+                    }
+                }
+            }
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write(pascalsTriangle[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}

ce41bba40864acaacca41a7bc80e240698677955
Stoycho Sirakov <stoycho.sirakov@gmail.com>
2023-10-30 23:48:13 +0200

    EX3.60/100

--- 02.MultidimensionalArrays/EX2.SquaresInMatrix/Program.cs
+++ 02.MultidimensionalArrays/EX2.SquaresInMatrix/Program.cs
@@ -8,12 +8,11 @@
                 .Split(" ")
                 .Select(int.Parse)
                 .ToArray();
-            char[,] matrix = new char[matrixDimensions[0], matrixDimensions[1]];
+            string[,] matrix = new string[matrixDimensions[0], matrixDimensions[1]];
             for (int i = 0; i < matrixDimensions[0]; i++)
             {
                 var input = Console.ReadLine()
                     .Split(" ")
-                    .Select(char.Parse)
                     .ToArray();
                 for (int y = 0; y < input.Length; y++)
                 {
@@ -22,11 +21,13 @@
             }
             //solution->
             int counter = 0;
-            for (int row = 0; row < matrixDimensions[0]-1; row++)
+            for (int row = 0; row < matrixDimensions[0] - 1; row++)
             {
-                for (int col = 1; col < matrixDimensions[1]; col++)
+                for (int col = 0; col < matrixDimensions[1] - 1; col++)
                 {
-                    if (matrix[row,col] == matrix[row,col-1] && matrix[row + 1, col] == matrix[row + 1, col - 1] && matrix[row + 1, col] == matrix[row, col])
+                    if (matrix[row, col] == matrix[row, col + 1] &&
+                        matrix[row, col] == matrix[row + 1, col] &&
+                        matrix[row, col] == matrix[row + 1, col + 1])
                     {
                         counter++;
                     }
---
+++ 02.MultidimensionalArrays/EX3.MaximalSum/Program.cs
@@ -0,0 +1,71 @@
+﻿namespace EX3.MaximalSum
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var matrixDimensions = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToArray();
+            int[,] matrix = new int[matrixDimensions[0], matrixDimensions[1]];
+            for (int i = 0; i < matrixDimensions[0]; i++)
+            {
+                var input = Console.ReadLine()
+                    .Split(" ")
+                    .Select(int.Parse)
+                    .ToArray();
+                for (int y = 0; y < input.Length; y++)
+                {
+                    matrix[i, y] = input[y];
+                }
+            }
+            // solution-->
+            List<int[,]> variants = new List<int[,]>();
+            for (int row = 0; row < matrixDimensions[0] - 2; row++)
+            {
+                for (int col = 0; col < matrixDimensions[1] - 2; col++)
+                {
+                    int[,] innerMatrix = new int[3, 3];
+                    for (int i = 0; i < 3; i++)
+                    {
+                        for (int j = 0; j < 3; j++)
+                        {
+                            innerMatrix[i, j] = matrix[row + i, col + j];
+                        }
+                    }
+                    variants.Add(innerMatrix);
+                }
+            }
+            int sum = 0;
+            int index = 0;
+            int innerIndex = -1;
+            foreach (var element in variants)
+            {
+                int innerSum = 0;
+                for(int row = 0; row < element.GetLength(0); row++)
+                {
+                    for (int col = 0; col < element.GetLength(1); col++)
+                    {
+                        innerSum += element[row,col];
+                    }
+                }
+                innerIndex++;
+                if (innerSum > sum)
+                {
+                    sum = innerSum;
+                    index = innerIndex;
+                }
+            }
+            Console.WriteLine($"Sum = {sum}");
+            for (int i = 0; i < variants[index].GetLength(0); i++)
+            {
+                for (int y = 0; y < variants[index].GetLength(1); y++)
+                {
+                    Console.Write($"{variants[index][i,y]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}

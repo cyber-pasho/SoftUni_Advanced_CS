f57b02f6b574e919c0687f21d720b425a8fe9d30
Stoycho Sirakov <stoycho.sirakov@gmail.com>
2023-11-17 22:12:25 +0200

    EX4.80/100

--- 02.MultidimensionalArrays/EX4.MatrixShuffling/Program.cs
+++ 02.MultidimensionalArrays/EX4.MatrixShuffling/Program.cs
@@ -4,7 +4,68 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var matrixDimensions = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            string[,] matrix = new string[matrixDimensions[0], matrixDimensions[1]];
+            for (int i = 0; i < matrixDimensions[0]; i++)
+            {
+                var input = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                for (int y = 0; y < input.Length; y++)
+                {
+                    matrix[i, y] = input[y];
+                }
+            }
+            //solution--->
+            string statement;
+            while ((statement = Console.ReadLine()) != "END")
+            {
+                var command = statement
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                if (command[0] == "swap")
+                {
+                    //deeper--->
+                    if (command.Length != 5)
+                    {
+                        Console.WriteLine("Invalid input!");
+                    }
+                    string commandValid = command[0];
+                    int row1 = int.Parse(command[1]);
+                    int col1 = int.Parse(command[2]);
+                    int row2 = int.Parse(command[3]);
+                    int col2 = int.Parse(command[4]);
+
+                    if  (row1 >= 0 && row1 <= matrixDimensions[0] 
+                     && row2 >= 0 && row2 <= matrixDimensions[0]
+                     && col1 >= 0 && col1 <= matrixDimensions[1]
+                     && col2 >= 0 && col2 <= matrixDimensions[1])
+
+                    {
+                        string swapEntry = matrix[row2, col2];
+                        matrix[row2, col2] = matrix[row1, col1];
+                        matrix[row1, col1] = swapEntry;
+                        //print--->
+                        for (int i = 0; i < matrix.GetLength(0); i++)
+                        {
+                            for (int x = 0; x < matrix.GetLength(1); x++)
+                            {
+                                Console.Write($"{matrix[i,x]} ");
+                            }
+                            Console.WriteLine();
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input!");
+                }
+            }
         }
     }
 }

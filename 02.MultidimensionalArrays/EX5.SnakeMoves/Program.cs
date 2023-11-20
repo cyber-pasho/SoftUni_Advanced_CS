4813f5d4ea2e86047c641d762040fce20f9f69c6
Stoycho Sirakov <stoycho.sirakov@gmail.com>
2023-11-21 00:00:40 +0200

    EX5.100/100

---
+++ 02.MultidimensionalArrays/EX5.SnakeMoves/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace EX5.SnakeMoves
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var matrixDimensions = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            char[,] matrix = new char[matrixDimensions[0], matrixDimensions[1]];
+            var input = Console.ReadLine();
+            int counter = 0;
+            int rowCounter = 0;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                if (counter == input.Length)
+                {
+                    counter = 0;
+                }
+                if (rowCounter % 2 != 0)
+                {
+                    for (int y = matrix.GetLength(1)-1; y >= 0; y--)
+                    {
+                        if (counter == input.Length)
+                        {
+                            counter = 0;
+                        }
+                        matrix[i, y] = input[counter];
+                        counter++;
+                    }
+                }
+                else
+                {
+                    for (int y = 0; y < matrix.GetLength(1); y++)
+                    {
+                        if (counter == input.Length)
+                        {
+                            counter = 0;
+                        }
+                        matrix[i, y] = input[counter];
+                        counter++;
+                    }
+                }
+                rowCounter++;
+            }
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int x = 0; x < matrix.GetLength(1); x++)
+                {
+                    Console.Write($"{matrix[i, x]}");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}

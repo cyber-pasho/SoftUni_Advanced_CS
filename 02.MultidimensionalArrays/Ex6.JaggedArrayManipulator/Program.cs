e62b3705e53c8c67ce15a8aaccecd5bd858a5259
Stoycho Sirakov <stoycho.sirakov@gmail.com>
2023-11-29 00:49:17 +0200

    EX6.85/100

--- 02.MultidimensionalArrays/Ex6.JaggedArrayManipulator/Program.cs
+++ 02.MultidimensionalArrays/Ex6.JaggedArrayManipulator/Program.cs
@@ -6,51 +6,80 @@
         {
             int inputRows = int.Parse(Console.ReadLine());
             string input;
-            int[][] matrix = new int[inputRows][];
+            long[][] matrix = new long[inputRows][];
             //
             while ((input = Console.ReadLine()) != "End")
             {
-                //asign-->
-                for (int i = 0; i < inputRows; i++)
+                var inputLines = input
+                        .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                if (inputLines[0] != "Add" && inputLines[0] != "Subtract")
                 {
-                    var inputLines = input
-                        .Split(" ", StringSplitOptions.RemoveEmptyEntries)
-                        .Select(int.Parse)
-                        .ToArray();
-                    matrix[i] = new int[inputLines.Length];
-                    for (int z = 0; z < inputLines.Length; z++)
+                    //assign -->
+                    for (int i = 0; i < inputRows; i++)
                     {
-                        matrix[i][z] = inputLines[z];
-                    }
-                    input = Console.ReadLine();
-                }
-                //asign<--
-                //analyze-->
-                for (int j = 1; j < inputRows; j++)
-                {
-                    if (matrix[j].Length == matrix[j-1].Length)
-                    {
-                        for (int y = j-1; y <= j; y++)
+                        matrix[i] = new long[inputLines.Length];
+                        for (int z = 0; z < inputLines.Length; z++)
                         {
-                            for (int k = 0; k < matrix[y].Length; k++)
+                            matrix[i][z] = int.Parse(inputLines[z]);
+                        }
+                        inputLines = Console.ReadLine().Split();
+                    }
+                    //assign <--
+                    //analyze -->
+                    for (int j = 1; j < inputRows; j++)
+                    {
+                        if (matrix[j].Length == matrix[j - 1].Length)
+                        {
+                            for (int y = j - 1; y <= j; y++)
                             {
-                                matrix[y][k] *= 2;
+                                for (int k = 0; k < matrix[y].Length; k++)
+                                {
+                                    matrix[y][k] *= 2;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            for (int q = j - 1; q <= j; q++)
+                            {
+                                for (int w = 0; w < matrix[q].Length; w++)
+                                {
+                                    matrix[q][w] /= 2;
+                                }
                             }
                         }
                     }
-                    else
+                    if (inputLines[0] == "End")
                     {
-                        for (int q = j - 1; q <= j; q++)
-                        {
-                            for (int w = 0; w < matrix[q].Length; w++)
-                            {
-                                matrix[q][w] /= 2;
-                            }
-                        }
+                        break;
                     }
                 }
-                //analyze--<
+                //analyze <--
+                //commands -->
+                else if (inputLines[0] == "Add"
+                    && long.Parse(inputLines[1]) >= 0
+                    && long.Parse(inputLines[1]) < inputRows
+                    && long.Parse(inputLines[2]) < matrix[int.Parse(inputLines[1])].Length
+                    && long.Parse(inputLines[2]) >= 0)
+                {
+                    matrix[int.Parse(inputLines[1])][int.Parse(inputLines[2])] += long.Parse(inputLines[3]);
+                }
+                else if (inputLines[0] == "Subtract"
+                    && long.Parse(inputLines[1]) >= 0
+                    && long.Parse(inputLines[1]) < inputRows
+                    && long.Parse(inputLines[2]) < matrix[int.Parse(inputLines[1])].Length
+                    && long.Parse(inputLines[2]) >= 0)
+                {
+                    matrix[int.Parse(inputLines[1])][int.Parse(inputLines[2])] -= long.Parse(inputLines[3]);
+                }
+             //commands <--
+             // print -->
             }
+            foreach (var row in matrix)
+            {
+                Console.WriteLine(string.Join(" ", row));
+            }
+            //print <--
         }
     }
 }

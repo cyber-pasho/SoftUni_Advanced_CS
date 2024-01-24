e05ad0958facc5f693e74a35a6cdd5c6bef63ff7
Stoycho Sirakov <stoycho.sirakov@gmail.com>
2024-01-24 23:37:32 +0200

    L01 + L02 100/100

--- 03.SetsAndDictionaries/L01.CountSameValuesInArray/Program.cs
+++ 03.SetsAndDictionaries/L01.CountSameValuesInArray/Program.cs
@@ -2,9 +2,28 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            var input = Console.ReadLine()
+                .Split(" ")
+                .Select(double.Parse)
+                .ToArray();
+            Dictionary<double, int> numbers = new Dictionary<double, int>();
+            foreach (var number in input)
+            {
+                if (!numbers.ContainsKey(number))
+                {
+                    numbers.Add(number, 1);
+                }
+                else 
+                { 
+                    numbers[number]++; 
+                }
+            }
+            foreach (var item in numbers)
+            {
+                Console.WriteLine($"{item.Key} - {item.Value} times");
+            }
         }
     }
 }
---
+++ 03.SetsAndDictionaries/L02.AvarageStudentGrades/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace L02.AvarageStudentGrades
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int size = int.Parse(Console.ReadLine());
+            Dictionary<string, List<decimal>> students = new Dictionary<string, List<decimal>>();
+            for (int i = 0; i < size; i++)
+            {
+                var input = Console.ReadLine()
+                    .Split();
+                string name = input[0];
+                decimal grade = decimal.Parse(input[1]);
+                if (!students.ContainsKey(name))
+                {
+                    students.Add(name, new List<decimal>() { grade });
+                }
+                else
+                {
+                    students[name].Add(grade);
+                }
+            }
+            foreach (var item in students)
+            {
+                Console.WriteLine($"{item.Key} -> {string.Join(" ", item.Value.Select(value => $"{value:F2}"))} (avg: {item.Value.Average():F2})");
+            }
+        }
+    }
+}

81d9d19ed0cd9ed2b80009df9a383e15f07851df
Stoycho Sirakov <stoycho.sirakov@gmail.com>
2024-01-28 22:56:11 +0200

    L03.100/100

---
+++ 03.SetsAndDictionaries/LargestNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Immutable;
+
+namespace LargestNumbers
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .OrderByDescending(n => n)
+                .ToArray();
+
+            if (input.Length < 3)
+            {
+                foreach (var n in input)
+                {
+                    Console.Write(n + " ");
+                }
+            }
+            else
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.Write(input[i] + " ");
+                }
+            }
+        }
+    }
+}

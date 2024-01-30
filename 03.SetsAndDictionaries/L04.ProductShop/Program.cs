0dd2c0e6b42cb00957f10cb35e0346d2c463cd64
Stoycho Sirakov <Stoycho.Sirakov@milaraint.com>
2024-01-30 16:56:30 +0200

    L04.Initial

---
+++ 03.SetsAndDictionaries/L04.ProductShop/Program.cs
@@ -0,0 +1,22 @@
+﻿string input;
+List<Shop> shops = new List<Shop>();
+while ((input = Console.ReadLine()) != "Revision")
+{
+    var data = input
+        .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+    string shopName = data[0];
+    string productName = data[1];
+    double productPrice = int.Parse(data[2]);
+
+}
+public class Shop
+{
+    public Shop(string name, Dictionary<string, double> product)
+    {
+        Name = name;
+        Product = product;
+    }
+
+    public string Name { get; set; }
+    public Dictionary<string, double> Product { get; set; }
+}

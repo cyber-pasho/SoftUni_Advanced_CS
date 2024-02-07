ef309bfb69b9c07d4bc0e01a2102e4918828fe17
Stoycho Sirakov <stoycho.sirakov@gmail.com>
2024-02-07 13:14:00 +0200

    EX02.100/100

---
+++ 03.SetsAndDictionaries/EX02.SetsOfElements/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace EX02.SetsOfElements
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var size = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToArray();
+            int size1 = size[0];
+            int size2 = size[1];
+            List<int> list1 = new List<int>();
+            List<int> list2 = new List<int>();
+            for (int i = 0; i < size1; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+                list1.Add(input);
+            }
+            for (int i = 0; i < size2; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+                list2.Add(input);
+            }
+            HashSet<int> set = new HashSet<int>();
+            foreach (var number in list1)
+            {
+                if (list2.Contains(number))
+                {
+                    set.Add(number);
+                }
+            }
+            foreach (var item in set)
+            {
+                Console.Write($"{item} ");
+            }
+        }
+    }
+}

1b5ab9aa011f41ecbc5534052a66127c14cfd620
Stoycho Sirakov <stoycho.sirakov@gmail.com>
2024-02-07 13:50:29 +0200

    EX04.100/100

---
+++ 03.SetsAndDictionaries/EX04.EvenTimes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+
+namespace EX04.EvenTimes
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int size = int.Parse(Console.ReadLine());
+            Dictionary<int,int> numbers = new Dictionary<int,int>();
+            for (int i = 0; i < size; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+                if (!numbers.ContainsKey(input))
+                {
+                    numbers.Add(input, 1);
+                }
+                else
+                {
+                    numbers[input]++;
+                }
+            }
+            var answer = numbers.FirstOrDefault(x=>x.Value%2==0);
+            Console.WriteLine(answer.Key);
+        }
+    }
+}

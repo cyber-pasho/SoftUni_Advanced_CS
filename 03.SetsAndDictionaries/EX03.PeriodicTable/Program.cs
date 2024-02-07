1675a12a59a01ebd0c4d0131ca395c08dd598914
Stoycho Sirakov <stoycho.sirakov@gmail.com>
2024-02-07 13:24:30 +0200

    EX03.100/100

---
+++ 03.SetsAndDictionaries/EX03.PeriodicTable/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace EX03.PeriodicTable
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int count = int.Parse(Console.ReadLine());
+            HashSet<string> elements = new HashSet<string>();
+            for (int i = 0; i < count; i++)
+            {
+                var input = Console.ReadLine()
+                    .Split(" ");
+                foreach (var element in input)
+                {
+                    elements.Add(element);
+                }
+            }
+            List<string> sortedElements = new List<string>(elements)
+                .OrderBy(x => x)
+                .ToList();
+            Console.WriteLine(string.Join(" ",sortedElements));
+        }
+    }
+}

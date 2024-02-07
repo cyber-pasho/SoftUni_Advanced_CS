44e7c050a430488f0d88c4faba4b3a5a9c0fa18d
Stoycho Sirakov <stoycho.sirakov@gmail.com>
2024-02-07 14:23:19 +0200

    EX05.100/100

---
+++ 03.SetsAndDictionaries/EX05.CountSymbols/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace EX05.CountSymbols
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            SortedDictionary<char, int> charsCount = new SortedDictionary<char, int>();
+            var input = Console.ReadLine();
+            foreach (var item in input)
+            {
+                if (!charsCount.ContainsKey(item))
+                {
+                    charsCount.Add(item, 1);
+                }
+                else
+                {
+                    charsCount[item]++;
+                }
+            }
+            foreach (var item in charsCount)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value} time/s");
+            }
+        }
+    }
+}

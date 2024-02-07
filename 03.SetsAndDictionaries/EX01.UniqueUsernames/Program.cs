70c34aec522a1947c5506b01652a25ef4c940f08
Stoycho Sirakov <stoycho.sirakov@gmail.com>
2024-02-07 13:01:34 +0200

    EX01.100/100

---
+++ 03.SetsAndDictionaries/EX01.UniqueUsernames/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace EX01.UniqueUsernames
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int count = int.Parse(Console.ReadLine());
+            HashSet<string> usernames = new HashSet<string>();
+            for (int i = 0; i < count; i++)
+            {
+                string input = Console.ReadLine();
+                usernames.Add(input);
+            }
+            foreach (var item in usernames)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}

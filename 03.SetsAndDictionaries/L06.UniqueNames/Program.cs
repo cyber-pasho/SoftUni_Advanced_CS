1759e1f9f120f724831de65f2589592857755d71
Stoycho Sirakov <stoycho.sirakov@gmail.com>
2024-02-07 10:22:58 +0200

    L07.100/100

--- 03.SetsAndDictionaries/L06.UniqueNames/Program.cs
+++ 03.SetsAndDictionaries/L06.UniqueNames/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main()
         {
-            
+            int count = int.Parse(Console.ReadLine());
+            HashSet<string> uniqueNames = new HashSet<string>();
+            string nameInput;
+            for (int i = 0; i < count; i++)
+            {
+                nameInput = Console.ReadLine();
+                uniqueNames.Add(nameInput);
+            }
+            foreach (string uniqueName in uniqueNames)
+            {
+                Console.WriteLine(uniqueName);
+            }
         }
     }
 }
---
+++ 03.SetsAndDictionaries/L07.ParkingLot/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace L07.ParkingLot
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string input;
+            HashSet<string> parking = new HashSet<string>();
+            while ((input = Console.ReadLine()) != "END")
+            {
+                var car = input.
+                    Split(", ", StringSplitOptions.RemoveEmptyEntries);
+                string direction = car[0];
+                string plateNumber = car[1];
+                switch (direction)
+                {
+                    case "IN":
+                        parking.Add(plateNumber);
+                        continue;
+                    case "OUT":
+                        parking.Remove(plateNumber);
+                        continue;
+                }
+            }
+            if (parking.Count == 0)
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+            else
+            {
+                foreach (var car in parking)
+                {
+                    Console.WriteLine(car);
+                }
+            }
+        }
+    }
+}

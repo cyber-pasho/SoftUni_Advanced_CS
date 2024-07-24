9781db84b49caa2d38f4c3b4c3e68159f610bb31
Stoycho Sirakov <stoycho.sirakov@gmail.com>
2024-07-24 21:38:35 +0300

    FunctioanProgrammingInitial

---
+++ 05.FunctionalProgramming/L01-SortEvenNumbers/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace L01_SortEvenNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

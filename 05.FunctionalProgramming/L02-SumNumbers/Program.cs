f4c6267aef3726f78b453b3beeb6dc96a123ba97
Stoycho Sirakov <stoycho.sirakov@gmail.com>
2024-07-24 23:14:20 +0300

    L01 + L02 100/100

---
+++ 05.FunctionalProgramming/L02-SumNumbers/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace L02_SumNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input  = Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToArray();
+
+            Console.WriteLine(input.Count());
+            Console.WriteLine(input.Sum());
+        }
+    }
+}

1573c505be5364cbf267c676f4eca234026dcae8
stoycho_work <stoycho.sirakov@gmail.com>
2024-09-27 14:51:53 +0300

    EX06 100/100

--- 05.FunctionalProgramming/EX06-ReverseandExclude/Program.cs
+++ 05.FunctionalProgramming/EX06-ReverseandExclude/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            
+            var input = Console.ReadLine()
+                .Split(" ",StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .Reverse()
+                .ToList();
+            int divisor = int.Parse(Console.ReadLine());
+            var filteredNumbers = input
+                .Where(num => !IsDivisible(num, divisor))
+                .ToList();
+            Console.WriteLine(string.Join(" ", filteredNumbers));
+        }
+        static bool IsDivisible(int number1, int number2)
+        {
+            return number1 % number2 == 0;
         }
     }
 }

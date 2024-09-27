cbb1a303a9b1218af707a2401432a8f6f5241876
stoycho_work <stoycho.sirakov@gmail.com>
2024-09-27 15:08:39 +0300

    EX07 100/100

---
+++ 05.FunctionalProgramming/EX07-PredicateforNames/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace EX07_PredicateforNames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nameLenght = int.Parse(Console.ReadLine());
+            var input = Console.ReadLine()
+                .Split(" ",StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+            var filteredNames = FilterCollection(input, name => name.Length <= nameLenght);
+            foreach (var item in filteredNames)
+            {
+                Console.Write(item);
+                Console.WriteLine();
+            }
+        }
+        private static List<string> FilterCollection(List<string> collection, Predicate<string> predicate)
+        {
+            return collection.FindAll(predicate);
+        }
+    }
+}

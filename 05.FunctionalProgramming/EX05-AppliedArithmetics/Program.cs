5fda4d7bcf105688ad4a17c2643a6bc8b114ccf8
stoycho_work <stoycho.sirakov@gmail.com>
2024-09-27 14:17:33 +0300

    EX05 100/100

---
+++ 05.FunctionalProgramming/EX05-AppliedArithmetics/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace EX05_AppliedArithmetics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+            string command;
+            while ( (command = Console.ReadLine()) != "end")
+            {
+                if (command == "add")
+                {
+                    nums = nums
+                        .Select(Add)
+                        .ToList();
+                }
+                else if (command == "multiply")
+                {
+                    nums = nums.Select(Multiply)
+                        .ToList();
+                }
+                else if (command == "subtract")
+
+                {
+                    nums = nums.Select(Subtract)
+                        .ToList();
+                }
+                else if(command == "print")
+                {
+                    Console.WriteLine(string.Join(" ", nums));
+                }
+                else
+                {
+                    Console.WriteLine("Wrong input");
+                }
+            }
+        }
+        public static int Add(int x)
+        {
+            return x + 1;
+        }
+        public static int Multiply(int x)
+        {
+            return x * 2;
+        }
+        public static int Subtract(int x)
+        {
+            return x - 1;
+        }
+    }
+}

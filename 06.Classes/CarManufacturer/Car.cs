606739c80d7e3a8d45c3d9add46658c81de59dec
stoycho_work <stoycho.sirakov@gmail.com>
2025-02-14 17:06:26 +0200

    L02 100/100

--- 06.Classes/CarManufacturer/Car.cs
+++ 06.Classes/CarManufacturer/Car.cs
@@ -6,22 +6,22 @@ using System.Threading.Tasks;
 
 namespace CarManufacturer
 {
-    public class Car
-    {
+	public class Car
+	{
 		private string make;
 
 		public string Make
 		{
-			get { return  make; }
-			set {  make = value; }
+			get { return make; }
+			set { make = value; }
 		}
 
 		private string model;
 
 		public string Model
 		{
-			get { return  model; }
-			set {  model = value; }
+			get { return model; }
+			set { model = value; }
 		}
 
 		private int year;
@@ -32,5 +32,42 @@ namespace CarManufacturer
 			set { year = value; }
 		}
 
+		private double fuelQuantity;
+
+		public double FuelQuantity
+		{
+			get { return fuelQuantity; }
+			set { fuelQuantity = value; }
+		}
+
+		private double fuelConsumption;
+
+		public double FuelConsumption
+		{
+			get { return fuelConsumption; }
+			set { fuelConsumption = value; }
+		}
+
+
+		public void Drive(int drive)
+		{
+			double remainingFuel = FuelQuantity - (fuelConsumption * drive);
+			if (remainingFuel > 0)
+			{
+				fuelQuantity = remainingFuel;
+			}
+			else
+			{
+				Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+
+		}
+		public void WhoAmI()
+		{
+				Console.WriteLine($"Make: {Make}");
+				Console.WriteLine($"Make: {Model}");
+				Console.WriteLine($"Make: {Year}");
+				Console.WriteLine($"Make: {FuelQuantity:F2}");
+        }
 	}
 }

83ed58ddda297ac74666e55e566611ea0110e64e
stoycho_work <stoycho.sirakov@gmail.com>
2025-02-25 11:52:01 +0200

    EX05 100/100

---
+++ 06.Classes/EX05.DateModifier/DataModifier.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX05.DateModifier
+{
+    public class DataModifier
+    {
+		private string firstDate;
+		private string secondDate;
+
+        public string FirstDate
+        {
+            get { return firstDate; }
+            set { firstDate = value; }
+        }
+
+        public string SecondDate
+        {
+            get { return secondDate; }
+            set { secondDate = value; }
+        }
+
+
+        public DataModifier(string firstDate, string secondDate)
+        {
+            this.FirstDate = firstDate;
+            this.SecondDate = secondDate;
+        }
+
+        public int DaysDifference ()
+        {
+            var start = firstDate
+                .Split(" ")
+                .Select(int.Parse)
+                .ToArray();
+            int year1 = start [0];
+            int month1 = start [1];
+            int day1 = start [2];
+            
+            //
+
+            var end = secondDate
+                .Split(" ")
+                .Select(int.Parse)
+                .ToArray();
+            int year2 = end[0];
+            int month2 = end[1];
+            int day2 = end[2];
+
+            DateTime date1 = new DateTime(year1, month1, day1);
+            DateTime date2 = new DateTime(year2, month2, day2);
+
+            // Calculate difference in days
+            int daysDifference = (date2 - date1).Days;
+
+            return daysDifference;
+        }
+    }
+}
---
+++ 06.Classes/EX05.DateModifier/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace EX05.DateModifier
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string date1 = string.Empty;
+            string date2 = string.Empty;
+            for (int i = 0; i <= 1; i++)
+            {
+                string input = Console.ReadLine();
+                if (i == 0)
+                {
+                    date1 = input;
+                }
+                else
+                {
+                    date2 = input;
+                }
+            }
+
+
+            DataModifier dates = new DataModifier(date1, date2);
+
+            int difference = dates.DaysDifference();
+
+            Console.WriteLine($"{Math.Abs(difference)}");
+        }
+    }
+}

defb3f58a168572c2306b10414f01e1e810b66c6
stoycho_work <stoycho.sirakov@gmail.com>
2025-08-19 16:41:15 +0300

    EX2 - 100/100

---
+++ 2-RecursiveFactorial/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+
+static long Factorial(int n)
+{
+    if (n == 0)
+    {
+        return 1;
+    }
+
+    return n*Factorial(n-1);
+}
+
+
+
+int input = int.Parse(Console.ReadLine());
+Console.WriteLine(Factorial(input));

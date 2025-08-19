9940dd3e357c21607671a45035ec540b9b692267
stoycho_work <stoycho.sirakov@gmail.com>
2025-08-19 16:14:32 +0300

    Ex.01 - 100/100

---
+++ 07.Algorithms/01-RecursiveArraySum/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _01_RecursiveArraySum
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Console.WriteLine(Sum(input, 0));
+            
+            static int Sum(int[] array, int index)
+            {
+                if (index == array.Length - 1)
+                {
+                    return array[index];
+                }
+
+                return array[index] + Sum(array, index+1);
+            }
+        }
+    }
+}
